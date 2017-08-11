6e9c491be79f897e6e51be1418352f813b66fa7c
mdafonte <mdafonte@sicfe.com.uy>
2017-08-11 11:43:15 -0300

    header y footer

--- portafolio/src/app/app.module.ts
+++ portafolio/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './componentes/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ portafolio/src/app/components/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styles: []
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+  year: number = new Date().getFullYear();
+
+  ngOnInit() {
+  }
+
+}
