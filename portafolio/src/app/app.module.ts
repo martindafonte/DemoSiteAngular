a0d3b198dd310f4cfec622f61ce6fdb5a25fe244
mdafonte <mdafonte@sicfe.com.uy>
2017-08-11 11:44:16 -0300

    porfolio component

--- portafolio/src/app/app.module.ts
+++ portafolio/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule
