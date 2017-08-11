c3df5ab739baa900e908afea3aed2bfa20a89035
mdafonte <mdafonte@sicfe.com.uy>
2017-08-11 13:55:15 -0300

    Componente item creado

--- portafolio/src/app/app.module.ts
+++ portafolio/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { AboutComponent } from './components/about/about.component';
+import { ItemsComponent } from './components/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    AboutComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule
