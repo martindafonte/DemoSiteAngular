2bd2451fe11b2278c88c859d61c605889d3394e6
mdafonte <mdafonte@sicfe.com.uy>
2017-08-11 15:12:22 -0300

    Info service created

--- portafolio/src/app/app.module.ts
+++ portafolio/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -8,7 +9,12 @@ import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { AboutComponent } from './components/about/about.component';
 import { ItemComponent } from './components/item/item.component';
 
+//rutas
 import { app_routing } from './app.routes';
+
+//servicios
+
+import { InformacionService } from './services/informacion.service';
 
 @NgModule({
   declarations: [
@@ -21,9 +27,10 @@ import { app_routing } from './app.routes';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     app_routing
   ],
-  providers: [],
+  providers: [InformacionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
