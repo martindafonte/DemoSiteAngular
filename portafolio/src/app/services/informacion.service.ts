af38001c8682f47749fe2f456a263466e47af304
mdafonte <mdafonte@sicfe.com.uy>
2017-08-14 11:10:46 -0300

    Servicio para firebase

--- portafolio/src/app/services/informacion.service.ts
+++ portafolio/src/app/services/informacion.service.ts
@@ -6,14 +6,30 @@ import { Http } from '@angular/http';
 export class InformacionService {
 
   info: any = {};
+  equipo: any = [];
   data_loaded: boolean = false;
+  sobre_nosotros_loaded: boolean = false;
 
   constructor(public http: Http) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+
+  public carga_info() {
     this.http.get("assets/data/info.pagina.json")
       .subscribe(data => {
         console.log(data.json());
         this.info = data.json();
         this.data_loaded = true;
+      });
+  }
+
+  carga_sobre_nosotros() {
+    this.http.get("https://demoappangular.firebaseio.com/equipo.json")
+      .subscribe(data => {
+        console.log(data.json());
+        this.equipo = data.json();
+        this.sobre_nosotros_loaded = true;
       });
   }
 
